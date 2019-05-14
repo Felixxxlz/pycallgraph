de3d634b5f597ae1f0ddeb962bec08d23627e303
YannLuo <yannluo@foxmail.com>
2019-05-14 13:47:02 +0800

    190514

--- exp3_numpy.py
+++ exp3_numpy.py
@@ -136,16 +136,16 @@ def main():
     "5e9c419a6d41eb9d544b6d87dd621ad5b346a0fa", "d89c4c7e7850578e5ee61e3e09abd86318906975",
     "632afad440193271535a33a89bc3e19c3ecc291c", "0c3c04ebc1f9b038ebc75b1dc0b46437accb3e7f",
     "81f0ddac64919e503beeea2c1812b36a607de55d", "d6dcaedad22f5842e28179351238b4847e74d5a9",
-    "e6147b9bf580361f2f74ac72003f81e957587528", "f297d317f9d2355228fb3a265e9ff3c69e37b1e2"][10:] # 别忘记安装对应版本的numpy环境
+    "e6147b9bf580361f2f74ac72003f81e957587528", "f297d317f9d2355228fb3a265e9ff3c69e37b1e2"][11:] # 别忘记安装对应版本的numpy环境
     for numpy_version in numpy_versions:
-        # os.chdir(os.path.join("..", "REPOS", "numpy"))
-        # p = Popen(["git", "checkout", "master"])
-        # p.communicate()
-        # p = Popen(["git", "checkout", numpy_version])
-        # p.communicate()
-        # p = Popen("sudo -S pip3 install .", shell=True, stdin=PIPE)
-        # p.communicate(bytes("Ly941122" + "\n", encoding="utf-8"))
-        # os.chdir(os.path.join("..", "..", "pycallgraph"))
+        os.chdir(os.path.join("..", "REPOS", "numpy"))
+        p = Popen(["git", "checkout", "master"])
+        p.communicate()
+        p = Popen(["git", "checkout", numpy_version])
+        p.communicate()
+        p = Popen("sudo -S pip3 install .", shell=True, stdin=PIPE)
+        p.communicate(bytes("Ly941122" + "\n", encoding="utf-8"))
+        os.chdir(os.path.join("..", "..", "pycallgraph"))
 
         downstream_test_pyfiles = os.listdir("test_numpy")
         ok = set()
