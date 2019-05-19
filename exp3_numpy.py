74148efdfd0d568a5f86c4c0f6b74e232b18aba5
YannLuo <yannluo@foxmail.com>
2019-05-19 20:04:22 +0800

    190519

--- exp3_numpy.py
+++ exp3_numpy.py
@@ -145,7 +145,7 @@ def main():
         p.communicate()
         p = Popen(["git", "checkout", numpy_version])
         p.communicate()
-        p = Popen("sudo -S pip3 install . --no-cache-dir", shell=True, stdin=PIPE)
+        p = Popen("sudo -S pip3 install .", shell=True, stdin=PIPE)
         p.communicate(bytes("Ly941122" + "\n", encoding="utf-8"))
         os.chdir(os.path.join("..", "..", "pycallgraph"))
 
